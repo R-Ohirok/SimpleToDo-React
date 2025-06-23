7fad037ab1f4095fc7b9d5372eb731ab50f438dd
Roman Ohirok <matsuhita.2307@gmail.com>
2025-06-23 19:10:40 +0300

    add skeleton

--- src/components/molecules/ToDoItem/ToDoItem.tsx
+++ src/components/molecules/ToDoItem/ToDoItem.tsx
@@ -1,9 +1,10 @@
 import type React from 'react';
 import styles from './ToDoItem.module.scss';
 import cn from 'classnames';
-import { memo, useCallback, useState } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import { normalizeValue } from '../../../utils/normalizeValue';
 import type { ToDoType } from '../../../types';
+import Skeleton from '@mui/material/Skeleton';
 
 interface Props {
   todo: ToDoType;
@@ -17,6 +18,12 @@ const ToDoItem: React.FC<Props> = memo(
     const { id, title, isCompleted } = todo;
 
     const [isEditing, setIsEditing] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+      const timer = setTimeout(() => setIsVisible(true), 1000);
+      return () => clearTimeout(timer);
+    }, []);
 
     const handleDelete = useCallback(() => onDelete(todo.id), []);
     const handleChangeStatus = useCallback(() => onChangeStatus(todo.id), []);
@@ -53,60 +60,72 @@ const ToDoItem: React.FC<Props> = memo(
 
     return (
       <li className={styles.todoItem}>
-        <input
-          name={id.toString()}
-          type="checkbox"
-          className={styles.todoItemStatus}
-          checked={isCompleted}
-          onChange={handleChangeStatus}
-        />
-
-        {isEditing ? (
-          <form
-            onBlur={handleEditTodo}
-            onSubmit={handleEditTodo}
-            onKeyUp={event => {
-              if (event.key === 'Escape') {
-                setIsEditing(false);
-              }
-            }}
-          >
-            <input
-              name="todoItemInput"
-              type="text"
-              className={styles.todoItemInput}
-              placeholder="Empty todo will be deleted"
-              defaultValue={title}
-              autoFocus
-            />
-          </form>
-        ) : (
+        {isVisible ? (
           <>
-            <span
-              className={cn(styles.todoItemTitle, {
-                [styles.todoItemTitleCompleted]: isCompleted,
-              })}
-            >
-              {title}
-            </span>
+            <input
+              name={id.toString()}
+              type="checkbox"
+              className={styles.todoItemStatus}
+              checked={isCompleted}
+              onChange={handleChangeStatus}
+            />
 
-            <div className={styles.todoItemControl}>
-              <button
-                className={cn(
-                  styles.todoItemControlBtn,
-                  styles.todoItemControlBtnEdit,
-                )}
-                onClick={handleSelectTodo}
-              ></button>
-              <button
-                className={cn(
-                  styles.todoItemControlBtn,
-                  styles.todoItemControlBtnDelete,
-                )}
-                onClick={handleDelete}
-              ></button>
-            </div>
+            {isEditing ? (
+              <form
+                onBlur={handleEditTodo}
+                onSubmit={handleEditTodo}
+                onKeyUp={event => {
+                  if (event.key === 'Escape') {
+                    setIsEditing(false);
+                  }
+                }}
+              >
+                <input
+                  name="todoItemInput"
+                  type="text"
+                  className={styles.todoItemInput}
+                  placeholder="Empty todo will be deleted"
+                  defaultValue={title}
+                  autoFocus
+                />
+              </form>
+            ) : (
+              <>
+                <span
+                  className={cn(styles.todoItemTitle, {
+                    [styles.todoItemTitleCompleted]: isCompleted,
+                  })}
+                >
+                  {title}
+                </span>
+
+                <div className={styles.todoItemControl}>
+                  <button
+                    className={cn(
+                      styles.todoItemControlBtn,
+                      styles.todoItemControlBtnEdit,
+                    )}
+                    onClick={handleSelectTodo}
+                  ></button>
+                  <button
+                    className={cn(
+                      styles.todoItemControlBtn,
+                      styles.todoItemControlBtnDelete,
+                    )}
+                    onClick={handleDelete}
+                  ></button>
+                </div>
+              </>
+            )}
           </>
+        ) : (
+          <Skeleton
+            variant="rectangular"
+            height={32}
+            width="100%"
+            sx={{ bgcolor: 'grey.500', borderRadius: '6px' }}
+            animation="wave"
+          />
         )}
       </li>
     );
