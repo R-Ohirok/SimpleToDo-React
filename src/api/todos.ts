c76fa9754d049de9f2b1cf5a9d298b4fc54a5710
Roman Ohirok <matsuhita.2307@gmail.com>
2025-06-24 17:53:30 +0300

    renaiming

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -4,7 +4,7 @@ export async function fetchTodos(): Promise<ToDoType[]> {
   const response = await fetch('http://localhost:3000/todos');
 
   if (!response.ok) {
-    throw new Error(`Помилка завантаження ToDo: ${response.statusText}`);
+    throw new Error(`Failed to download ToDos: ${response.statusText}`);
   }
 
   const data = await response.json();
