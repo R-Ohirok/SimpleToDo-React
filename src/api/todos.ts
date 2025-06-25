7845017d03b19d26b1fb3b9334dc9295bf394834
Roman Ohirok <matsuhita.2307@gmail.com>
2025-06-25 12:08:22 +0300

    feat: add functionality to create and delete ToDos on the server

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -11,3 +11,25 @@ export async function getTodos(): Promise<ToDoType[]> {
     throw error;
   }
 }
+
+export async function addTodo(newTodo: ToDoType): Promise<ToDoType[]> {
+  try {
+    const response = await instance.post('/todos', newTodo);
+
+    return response.data;
+  } catch (error) {
+    console.error('Failed to create ToDo: ', error);
+    throw error;
+  }
+}
+
+export async function deleteTodo(idToDelete: string): Promise<ToDoType[]> {
+  try {
+    const response = await instance.delete(`/todos/${idToDelete}`);
+
+    return response.data;
+  } catch (error) {
+    console.error('Failed to delete ToDo: ', error);
+    throw error;
+  }
+}
