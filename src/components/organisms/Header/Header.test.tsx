3624e5e1b6e349ab163bcd9495105ea4827017b5
Roman Ohirok <matsuhita.2307@gmail.com>
2025-07-10 10:24:51 +0300

    test: add more tests for header

--- src/components/organisms/Header/Header.test.tsx
+++ src/components/organisms/Header/Header.test.tsx
@@ -10,23 +10,23 @@ function ShowLocation() {
 }
 
 function renderWithRouter(initialEntries = ['/']) {
-    return render(
-      <MemoryRouter initialEntries={initialEntries}>
-        <Routes>
-          <Route
-            path="*"
-            element={
-              <>
-                <Header />
-                <ShowLocation />
-              </>
-            }
-          />
-        </Routes>
-      </MemoryRouter>,
-    );
-  };
-  
+  return render(
+    <MemoryRouter initialEntries={initialEntries}>
+      <Routes>
+        <Route
+          path="*"
+          element={
+            <>
+              <Header />
+              <ShowLocation />
+            </>
+          }
+        />
+      </Routes>
+    </MemoryRouter>,
+  );
+}
+
 describe('Header', () => {
   it('render header', () => {
     renderWithRouter();
@@ -47,40 +47,66 @@ describe('Header', () => {
   });
 
   it('update URL searchParams when search is submitted', async () => {
-      renderWithRouter();
-  
-      const search = screen.getByRole('button', { name: 'search' });
-      const input = screen.getByRole('textbox')  as HTMLInputElement;
+    renderWithRouter();
 
-      fireEvent.change(input, { target: { value: 'test' } });
-      expect(input.value).toBe('test');
+    const search = screen.getByRole('button', { name: 'search' });
+    const input = screen.getByRole('textbox') as HTMLInputElement;
 
-  
-      await userEvent.click(search);
-      expect(screen.getByTestId('location-display').textContent).toContain(
-        'title=test',
-      );
-    });
+    fireEvent.change(input, { target: { value: 'test' } });
+    expect(input.value).toBe('test');
+
+    await userEvent.click(search);
+    expect(screen.getByTestId('location-display').textContent).toContain(
+      'title=test',
+    );
+  });
 
   it('delete title from URL searchParams when clean button clicked', async () => {
-      renderWithRouter();
-  
-      const search = screen.getByRole('button', { name: 'search' });
-      const clean = screen.getByRole('button', { name: 'clean' });
-      const input = screen.getByRole('textbox')  as HTMLInputElement;
+    renderWithRouter();
 
-      fireEvent.change(input, { target: { value: 'test' } });
-      expect(input.value).toBe('test');
+    const search = screen.getByRole('button', { name: 'search' });
+    const clean = screen.getByRole('button', { name: 'clean' });
+    const input = screen.getByRole('textbox') as HTMLInputElement;
 
-  
-      await userEvent.click(search);
-      expect(screen.getByTestId('location-display').textContent).toContain(
-        'title=test',
-      );
+    fireEvent.change(input, { target: { value: 'test' } });
+    expect(input.value).toBe('test');
 
-      await userEvent.click(clean);
-      expect(screen.getByTestId('location-display').textContent).not.toContain(
-        'title=test',
-      );
-    });
+    await userEvent.click(search);
+    expect(screen.getByTestId('location-display').textContent).toContain(
+      'title=test',
+    );
+
+    await userEvent.click(clean);
+    expect(screen.getByTestId('location-display').textContent).not.toContain(
+      'title=test',
+    );
+  });
+
+  it('update dropdown value on selection', () => {
+    renderWithRouter();
+
+    const dropdown = screen.getByRole('combobox');
+    fireEvent.change(dropdown, { target: { value: 'Completed' } });
+
+    expect(dropdown).toHaveValue('Completed');
+  });
+
+  it('update URL searchParams when change dropdown value', () => {
+    renderWithRouter();
+
+    const dropdown = screen.getByRole('combobox');
+    fireEvent.change(dropdown, { target: { value: 'Completed' } });
+
+    expect(dropdown).toHaveValue('Completed');
+    expect(screen.getByTestId('location-display').textContent).toContain(
+      'status=Completed',
+    );
+
+    fireEvent.change(dropdown, { target: { value: 'Active' } });
+
+    expect(dropdown).toHaveValue('Active');
+    expect(screen.getByTestId('location-display').textContent).toContain(
+      'status=Active',
+    );
+  });
 });
