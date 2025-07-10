16e539b4bc68d7f8818a97740141c37249e7afa1
Roman Ohirok <matsuhita.2307@gmail.com>
2025-07-10 10:18:15 +0300

    test: add tests for header

---
+++ src/components/organisms/Header/Header.test.tsx
@@ -0,0 +1,86 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { MemoryRouter, Route, Routes, useLocation } from 'react-router-dom';
+import Header from './Header';
+import userEvent from '@testing-library/user-event';
+
+function ShowLocation() {
+  const location = useLocation();
+  return <div data-testid="location-display">{location.search}</div>;
+}
+
+function renderWithRouter(initialEntries = ['/']) {
+    return render(
+      <MemoryRouter initialEntries={initialEntries}>
+        <Routes>
+          <Route
+            path="*"
+            element={
+              <>
+                <Header />
+                <ShowLocation />
+              </>
+            }
+          />
+        </Routes>
+      </MemoryRouter>,
+    );
+  };
+  
+describe('Header', () => {
+  it('render header', () => {
+    renderWithRouter();
+
+    expect(screen.getByText('TODO LIST')).toBeInTheDocument();
+    expect(screen.getAllByRole('button')).toHaveLength(3);
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    expect(screen.getByRole('combobox')).toBeInTheDocument();
+  });
+
+  it('allow typing in the search input', () => {
+    renderWithRouter();
+
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+
+    fireEvent.change(input, { target: { value: 'test' } });
+    expect(input.value).toBe('test');
+  });
+
+  it('update URL searchParams when search is submitted', async () => {
+      renderWithRouter();
+  
+      const search = screen.getByRole('button', { name: 'search' });
+      const input = screen.getByRole('textbox')  as HTMLInputElement;
+
+      fireEvent.change(input, { target: { value: 'test' } });
+      expect(input.value).toBe('test');
+
+  
+      await userEvent.click(search);
+      expect(screen.getByTestId('location-display').textContent).toContain(
+        'title=test',
+      );
+    });
+
+  it('delete title from URL searchParams when clean button clicked', async () => {
+      renderWithRouter();
+  
+      const search = screen.getByRole('button', { name: 'search' });
+      const clean = screen.getByRole('button', { name: 'clean' });
+      const input = screen.getByRole('textbox')  as HTMLInputElement;
+
+      fireEvent.change(input, { target: { value: 'test' } });
+      expect(input.value).toBe('test');
+
+  
+      await userEvent.click(search);
+      expect(screen.getByTestId('location-display').textContent).toContain(
+        'title=test',
+      );
+
+      await userEvent.click(clean);
+      expect(screen.getByTestId('location-display').textContent).not.toContain(
+        'title=test',
+      );
+    });
+});
